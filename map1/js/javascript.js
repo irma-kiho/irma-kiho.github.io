2133fdade73a960982f9014c854f0169671bdb06
irma-kiho <irmaleene@gmail.com>
2026-05-05 15:45:44 +0300

    Update javascript.js

--- map1/js/javascript.js
+++ map1/js/javascript.js
@@ -57,8 +57,8 @@ async function addDistrictsGeoJson(url) {
 async function addCelltowersGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
-  const circles = L.geoJson(data, {
-	pointToLayer: createCircle,
-  })
-  circles.addTo(map)
+  const markers = L.geoJson(data)
+  const clusters = L.markerClusterGroup()
+  clusters.addLayer(markers)
+  clusters.addTo(map)
 } addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
