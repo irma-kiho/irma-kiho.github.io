6bfbd5f2ea89056db3a182ccf92e5946d7750981
irma-kiho <irmaleene@gmail.com>
2026-05-05 14:03:08 +0300

    Update javascript.js

--- map1/js/javascript.js
+++ map1/js/javascript.js
@@ -6,10 +6,17 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 })
 
 osm.addTo(map)
+
+function popUPinfo(feature, layer) {
+  layer.bindPopup(feature.properties.NIMI)
+}
+
 async function addDistrictsGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
-  const polygons = L.geoJson(data)
+  const polygons = L.geoJson(data, {
+    onEachFeature: popUPinfo,
+  })
   polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
